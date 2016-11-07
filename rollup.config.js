707c08401bca02efbb27b7abbb116dd96cde797b
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2016-11-07 05:28:02 +0200

    drop linting as part of bundling

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,8 @@
 import babel from 'rollup-plugin-babel';
-import eslint from 'rollup-plugin-eslint';
-
-let pkg = require('./package.json');
-let version = pkg.version;
 
 export default {
   entry: 'index.js',
   plugins: [
-    eslint({
-      exclude: [
-        'example/**',
-      ]
-    }),
     babel(),
   ],
   targets: [
