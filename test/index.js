00664b05b2fa1247cb4345e9e2a3bdf72d21f4f2
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-02-24 14:14:51 +0200

    better test

--- test/index.js
+++ test/index.js
@@ -3,58 +3,76 @@
 const test = require('tape');
 const pager = require('../').createPager;
 
-test('will cycle through', (t) => {
-  const counter = pager(23, 17);
+const trash = [ undefined, null, Infinity, NaN, Object, Function, ' ', '\t\t', '\n\r', '-1', 1.1 ];
 
-  t.plan(2);
-
-  counter.prev();
-  t.equal(counter.nick(), 16, 'prev is a match');
-
-  counter.next();
-  counter.next();
-  t.equal(counter.nick(), 18, 'next is a match');
-});
-
-test('will pause if index or total misconfigured', (t) => {
-  const counter = pager(-1, -2);
-
-  t.plan(2);
-
-  counter.prev();
-  t.equal(counter.nick(), -2, 'prev has no effect');
-
-  counter.next();
-  t.equal(counter.nick(), -2, 'next has no effect');
-});
-
-test('will protect data', (t) => {
-  const counter1 = pager(2, 1);
-  const counter2 = pager(3, 2);
+test('will maintain own state', (t) => {
+  const counter1 = pager(1);
+  const counter2 = pager(1);
 
   counter1.prev();
   counter2.prev();
 
-  t.equal(counter1.nick(), 0, 'index is a match');
-  t.equal(counter2.nick(), 1, 'index is a match');
+  t.notDeepEqual(counter1, counter2);
+  t.equal(counter1.nick(), 0);
+  t.equal(counter2.nick(), 0);
 
   t.end();
 });
 
-test('will default silently when fed garbage', (t) => {
-  const garbage = [undefined, ' ', '', '\n', NaN, {}, [], Date()];
+test('will cycle through', (t) => {
+  const counter = pager(5);
 
-  garbage.forEach((v) => {
-    const counter = pager(v);
-    const methods = Object.keys(counter)
-      .filter(key => typeof counter[key] === 'function')
-      .map(key => counter[key])
-      .forEach(val => t.doesNotThrow(val));
+  t.plan(2);
 
-    t.equal(counter.pick(v), 0, 'cannot mess with index');
-    t.equal(counter.total(v), 0, 'cannot mess with total');
-  });
+  for (let i = 0, total = counter.total(); i < total; i += 1) {
+    counter.next();
+  }
 
+  t.equal(counter.nick(), 0);
+
+  for (let i = 0, total = counter.total(); i < total; i += 1) {
+    counter.prev();
+  }
+
+  t.equal(counter.nick(), 0);
+});
+
+test('will respond but default when thrown trash at', (t) => {
+  const counter = pager(null);
+  const results = trash.reduce((acc, val) => {
+    return acc + counter.total(val) + counter.pick(val);
+  }, 0);
+
+  t.equal(results, 0);
+  t.end();
+});
+
+test('will only allow positive finite numbers', (t) => {
+  const counter = pager(-1, -1);
+
+  t.equal(counter.total(-8e5), 0);
+  t.equal(counter.nick(), 0);
+  t.equal(counter.pick(Infinity), 0);
+  t.end();
+});
+
+test('will pause if', (t) => {
+  // Both total and index are zero
+  let counter = pager();
+
+  counter.next();
+  counter.next();
+
+  t.equal(counter.nick(), 0, 'index and total are zero');
+
+  // Total less than index
+  counter = pager(0, 10);
+
+  counter.next();
+  counter.next();
+  counter.total(1);
+
+  t.equal(counter.nick() + counter.total(), 10, 'index greater than total');
   t.end();
 });
 
