947556a5ac8b140cb5860f0a231e10bd21863d23
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-06-21 13:18:26 +0200

    test: rework/reword per latest amends

--- test/index.js
+++ test/index.js
@@ -1,76 +1,74 @@
-'use strict';
-
 const test = require('tape');
-const pager = require('../').createPager;
+const createPager = require('../');
 
-const trash = [ undefined, null, Infinity, NaN, Object, Function, ' ', '\t\t', '\n\r', '-1', 1.1 ];
+const trash = [undefined, null, Infinity, NaN, Object, Function, ' ', '\t\t', '\n\r', '-1', 1.1];
 
 test('will maintain own state', (t) => {
-  const counter1 = pager(1);
-  const counter2 = pager(1);
+  const pager1 = createPager(1);
+  const pager2 = createPager(1);
 
-  counter1.prev();
-  counter2.prev();
+  pager1.prev();
+  pager2.prev();
 
-  t.notDeepEqual(counter1, counter2);
-  t.equal(counter1.nick(), 0);
-  t.equal(counter2.nick(), 0);
+  t.notDeepEqual(pager1, pager2);
+  t.equal(pager1.nick(), 0);
+  t.equal(pager2.nick(), 0);
 
   t.end();
 });
 
 test('will cycle through', (t) => {
-  const counter = pager(5);
+  const pager = createPager(5);
 
   t.plan(2);
 
-  for (let i = 0, total = counter.total(); i < total; i += 1) {
-    counter.next();
+  for (let i = 0, total = pager.total(); i < total; i += 1) {
+    pager.next();
   }
 
-  t.equal(counter.nick(), 0);
+  t.equal(pager.nick(), 0);
 
-  for (let i = 0, total = counter.total(); i < total; i += 1) {
-    counter.prev();
+  for (let i = 0, total = pager.total(); i < total; i += 1) {
+    pager.prev();
   }
 
-  t.equal(counter.nick(), 0);
+  t.equal(pager.nick(), 0);
 });
 
 test('will respond but default when thrown trash at', (t) => {
-  const counter = pager(null);
-  const results = trash.reduce((acc, val) => acc + counter.total(val) + counter.pick(val), 0);
+  const pager = createPager(null);
+  const results = trash.reduce((acc, val) => acc + pager.total(val) + pager.pick(val), 0);
 
   t.equal(results, 0);
   t.end();
 });
 
 test('will only allow positive finite numbers', (t) => {
-  const counter = pager(-1, -1);
+  const pager = createPager(-1, -1);
 
-  t.equal(counter.total(-8e5), 0);
-  t.equal(counter.nick(), 0);
-  t.equal(counter.pick(Infinity), 0);
+  t.equal(pager.total(-8e5), 0);
+  t.equal(pager.nick(), 0);
+  t.equal(pager.pick(Infinity), 0);
   t.end();
 });
 
 test('will pause if', (t) => {
   // Both total and index are zero
-  let counter = pager();
+  let pager = createPager();
 
-  counter.next();
-  counter.next();
+  pager.next();
+  pager.next();
 
-  t.equal(counter.nick(), 0, 'index and total are zero');
+  t.equal(pager.nick(), 0, 'index and total are zero');
 
   // Total less than index
-  counter = pager(0, 10);
+  pager = createPager(0, 10);
 
-  counter.next();
-  counter.next();
-  counter.total(1);
+  pager.next();
+  pager.next();
+  pager.total(1);
 
-  t.equal(counter.nick() + counter.total(), 10, 'index greater than total');
+  t.equal(pager.nick() + pager.total(), 10, 'index greater than total');
   t.end();
 });
 
