e44c30d13d8638a0313ff459204f4326bc47f838
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2020-01-27 00:08:45 +0000

    test: fix for tapeless latest

--- test.js
+++ test.js
@@ -11,9 +11,13 @@ const p2 = createPager(1)
 p1.prev()
 p2.prev()
 
-notDeepEqual(p1, p2, 'pagers are different', 'will maintain own state')
-equal(p1.nick(), 0)
-equal(p2.nick(), 0)
+notDeepEqual
+  .describe('pagers are different', 'will maintain own state')
+  .test(p1, p2)
+
+equal
+  .test(p1.nick(), 0)
+  .test(p2.nick(), 0)
 
 const p3 = createPager(5)
 
@@ -21,24 +25,32 @@ for (let i = 0, total = p3.total(); i < total; i += 1) {
   p3.next()
 }
 
-equal(p3.nick(), 0, 'back to zero', 'will cycle through')
+equal
+  .describe('back to zero', 'will cycle through')
+  .test(p3.nick(), 0)
 
 for (let i = 0, total = p3.total(); i < total; i += 1) {
   p3.prev()
 }
 
-equal(p3.nick(), 0, 'back to zero')
+equal
+  .describe('back to zero')
+  .test(p3.nick(), 0)
 
 const p4 = createPager(null)
 const results = trash.reduce((acc, val) => acc + p4.total(val) + p4.pick(val), 0)
 
-equal(results, 0, 'nothing', 'will respond but default when thrown trash at')
+equal
+  .describe('nothing', 'will respond but default when thrown trash at')
+  .test(results, 0)
 
 const p5 = createPager(-1, -1)
 
-equal(p5.total(-8e5), 0, 'total is a match', 'will only allow positive finite numbers')
-equal(p5.nick(), 0)
-equal(p5.pick(Infinity), 0)
+equal
+  .describe('total is a match', 'will only allow positive finite numbers')
+  .test(p5.total(-8e5), 0)
+  .test(p5.nick(), 0)
+  .test(p5.pick(Infinity), 0)
 
 // Both total and index are zero
 let pager = createPager()
@@ -46,7 +58,9 @@ let pager = createPager()
 pager.next()
 pager.next()
 
-equal(pager.nick(), 0, 'index and total are zero', 'will pause if')
+equal
+  .describe('index and total are zero', 'will pause if')
+  .test(pager.nick(), 0)
 
 // Total less than index
 pager = createPager(0, 10)
@@ -55,4 +69,6 @@ pager.next()
 pager.next()
 pager.total(1)
 
-equal(pager.nick() + pager.total(), 10, 'index greater than total')
+equal
+  .describe('index greater than total')
+  .test(pager.nick() + pager.total(), 10)
